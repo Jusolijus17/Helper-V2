33a27f9326f7d8ae2d01ae2502875485d7d22255
Justin Lefrançois <justinlefrancois@mbp-de-justin.local>
2018-04-29 00:11:19 -0400

    Initial Commit

---
+++ Helper V2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Helper V2
+//
+//  Created by Justin Lefrançois on 18-04-29.
+//  Copyright © 2018 Justin Lefrançois. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
